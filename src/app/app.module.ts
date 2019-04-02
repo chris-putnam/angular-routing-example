4570efd2a30e49c19732a7a064d812affe581057
Chris Putnam <chris@christopher-putnam.com>
2019-04-02 00:02:20 -0400

    add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { ClientManagementComponent } from './client-management/client-management.component';
 import { ClientMaintenanceComponent } from './client-maintenance/client-maintenance.component';
 import { ClientDataService } from './client-data/client-data.service';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ClientManagementComponent,
     ClientMaintenanceComponent,
+    NavbarComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [ClientDataService],
